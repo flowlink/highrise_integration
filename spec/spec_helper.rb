1fcfa1ec09dcaab0227c7f4f2f2b5ff6c81cb659
Washington Luiz <huoxito@gmail.com>
2014-07-30 16:18:17 -0300

    Dont allow http connection when no cassetes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ def app
 end
 
 VCR.configure do |config|
-  config.allow_http_connections_when_no_cassette = true
+  config.allow_http_connections_when_no_cassette = false
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.default_cassette_options = { match_requests_on: [:method, :path] }
   config.hook_into :webmock
