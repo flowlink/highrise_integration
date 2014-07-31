5dd8820c6e2dad2a5b8547afc26cdc49430742c8
Washington Luiz <huoxito@gmail.com>
2014-07-31 16:02:34 -0300

    Only set credentials when they're present

--- highrise_endpoint.rb
+++ highrise_endpoint.rb
@@ -1,4 +1,3 @@
-Bundler.require(:default)
 require "endpoint_base/sinatra"
 
 module HighriseEndpoint
@@ -12,8 +11,10 @@ module HighriseEndpoint
 
     before do
       # Sets the Highrise credentials based on what is provided
-      Highrise::Base.site = @config["highrise_site_url"]
-      Highrise::Base.user = @config["highrise_api_token"]
+      if @config.is_a? Hash
+        Highrise::Base.site = @config["highrise_site_url"]
+        Highrise::Base.user = @config["highrise_api_token"]
+      end
     end
 
     def line_items_to_string(line_items)
@@ -196,81 +197,6 @@ module HighriseEndpoint
       end
     end
 
-    def handle_shipment(payload)
-      @shipment = payload[:shipment]
-
-      deals = Highrise::Deal.all
-      deals = deals.map{ |deal|
-        deal if deal.name == "Order ##{@shipment[:order_id]}"
-      }.compact
-
-      @deal = deals.first
-
-      person_tasks = if @payload[:shipment][:highrise_tasks] && @payload[:shipment][:highrise_tasks][:person]
-        @payload[:shipment][:highrise_tasks][:person]
-      else
-        []
-      end
-
-      deal_tasks = if @payload[:shipment][:highrise_tasks] && @payload[:shipment][:highrise_tasks][:deal]
-        @payload[:shipment][:highrise_tasks][:deal]
-      else
-        []
-      end
-
-      if @deal
-        address = @shipment[:shipping_address]
-
-        formatted_address = <<-FORMATTED_ADDRESS
-#{address[:firstname]} #{address[:lastname]}
-#{address[:address1]}
-#{address[:address2]}
-#{address[:city]}, #{address[:state]}, #{address[:country]} #{address[:zipcode]}
-FORMATTED_ADDRESS
-
-        shipment_body = <<-SHIPMENT_BODY
-Tracking: #{@shipment[:tracking] ? @shipment[:tracking] : "No tracking code for this shipment."}
-
-Shipped to:
-#{formatted_address}
-
-Manifest:
-#{line_items_to_string(@shipment[:items])}
-
-Shipped On: #{@shipment[:shipped_at] ? @shipment[:shipped_at] : "Not yet shipped."}
-SHIPMENT_BODY
-
-        @note = Highrise::Note.create(body: shipment_body, subject_id: @deal.id, subject_type: "Deal")
-
-        person_tasks.each do |person_task|
-          if ["today", "tomorrow", "this_week", "next_week", "later"].include?(person_task[:due])
-            highrise_task = Highrise::Task.new(body: person_task[:body], frame: person_task[:due], subject_type: "Party", subject_id: @deal.party.id, owner_id: person_task[:assigned_to])
-          else
-            highrise_task = Highrise::Task.new(body: person_task[:body], frame: "specific", due_at: person_task[:due], subject_type: "Party", subject_id: @deal.party.id, owner_id: person_task[:assigned_to])
-          end
-
-          highrise_task.save
-        end
-
-        deal_tasks.each do |deal_task|
-          if ["today", "tomorrow", "this_week", "next_week", "later"].include?(deal_task[:due])
-            highrise_task = Highrise::Task.new(body: deal_task[:body], frame: deal_task[:due], subject_type: "Deal", subject_id: @deal.id, owner_id: deal_task[:assigned_to])
-          else
-            highrise_task = Highrise::Task.new(body: deal_task[:body], frame: "specific", due_at: deal_task[:due], subject_type: "Deal", subject_id: @deal.id, owner_id: deal_task[:assigned_to])
-          end
-
-          highrise_task.save
-        end
-
-
-        if @note.save
-          result 200, "Shipment info was added to deal: #{@deal.name}"
-        else
-          result 500, @note.errors[:base].join(", ")
-        end
-      end
-    end
-
     ["/add_customer", "/update_customer"].each do |path|
       post path do
         handle_customer @payload
@@ -280,12 +206,6 @@ SHIPMENT_BODY
     ["/add_order", "/update_order"].each do |path|
       post path do
         handle_order @payload
-      end
-    end
-
-    ["/add_shipment", "/update_shipment"].each do |path|
-      post path do
-        handle_shipment @payload
       end
     end
   end
